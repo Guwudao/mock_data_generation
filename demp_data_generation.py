ba681bfb38f59299780d96bd58aa55576b4b424e
Guwudao <313640893@qq.com>
2022-09-05 21:11:55 +0800

    1. separate data generator and config loader

---
+++ demp_data_generation.py
@@ -0,0 +1,97 @@
+import random
+import datetime
+import time
+import numpy as np
+
+
+class Generator:
+
+    def __init__(self):
+        self.field = None
+        self.temp_date = []
+
+    def generate_date(self, is_time=False, base=0, is_early=False):
+        now = datetime.datetime.now()
+        start_time = (2018, 1, 1, 0, 0, 0, 0, 0, 0)
+        end_time = (now.year, now.month, now.day - 1, 23, 59, 59, 0, 0, 0)
+        start = int(time.mktime(start_time))
+        end = int(time.mktime(end_time))
+        t = random.randint(start, end)
+        if is_early:
+            self.temp_date.append(t)
+
+        if is_time:
+            return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t))
+
+        if base == 0:
+            return time.strftime("%Y%m%d", time.localtime(t))
+        else:
+            return time.strftime("%Y%m%d", time.localtime(base + random.randint(100000, 30000000)))
+
+    def email(self):
+        list_sum = [i for i in range(10)] + ["a", "b", "c", "d", "e", "f", "g", "h", 'i', "j", "k",
+                                             "l", "M", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                                             "w", "x", "y", "z"]
+        email_str = ""
+        email_suffix = ["@163.com", "@qq.com", "@gmail.com", "@mail.hk.com", "＠yahoo.co.id", "@mail.com"]
+        for i in range(10):
+            a = str(random.choice(list_sum))
+            email_str = email_str + a
+        return email_str + random.choice(email_suffix)
+
+    def mobile(self):
+        mobiles = ["139", "138", "137", "136", "135", "134", "159", "158", "157", "150", "151", "152", "188", "187",
+                   "182", "183", "184", "178", "130", "131", "132", "156", "155", "186", "185", "176", "133", "153",
+                   "189", "180", "181", "177"]
+        number = "0123456789"
+        mobile = random.choice(mobiles) + "".join(random.choice(number) for i in range(8))
+        return mobile
+
+    def date(self):
+        return self.generate_date()
+
+    def time(self):
+        return self.generate_date(is_time=True)
+
+    def early_date(self):
+        return self.generate_date(is_early=True)
+
+    def late_date(self):
+        # late_date must be generated after early_date
+        base = self.temp_date.pop(0)
+        return self.generate_date(base=base)
+
+    def default(self):
+        print("no implement for this field: ", self.field)
+
+    def list_generation(self):
+        return self.field[np.random.randint(0, len(self.field))]
+
+    def dict_generation(self):
+        opera_type = self.field["type"]
+        start = self.field["start"]
+        end = self.field["end"]
+
+        operator_gen = {
+            "int": random.randint,
+            "float": random.uniform
+        }
+        operator = operator_gen[opera_type]
+        return operator(start, end)
+
+    def generate(self, f):
+        self.field = f
+        try:
+            if isinstance(f, str):
+                fun = getattr(self, f.lower(), self.default)
+                return fun()
+            elif isinstance(f, list):
+                return self.list_generation()
+            elif isinstance(f, dict):
+                return self.dict_generation()
+            else:
+                print("Unsupport data type")
+        except Exception as e:
+            print("error with: ", f, e)
+
+        
---
+++ dmp_config_loader.py
@@ -0,0 +1,105 @@
+import json
+import uuid
+import pandas as pd
+import datetime
+import os
+import oss2
+from demp_data_generation import Generator
+
+
+class Mock:
+
+    def __init__(self, total_id, data, tables):
+        self.hk_ids = [uuid.uuid4().hex for i in range(total_id)]
+        self.data = data
+        self.tables = tables
+        self.temp_date = []
+        self.path = "./data/dmp_mock"
+
+    def get_time(self):
+        t = datetime.datetime.now()
+        now = t.strftime("%Y-%m-%d %H:%M:%S")
+        return now
+
+    def get_fields_and_count(self, table):
+        return self.data[table]["fields"], self.data[table]["count"]
+
+    def create_dataframe(self, *args):
+        table_name, fields, values = args
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+        dataframe = {}
+        for field, value in zip(fields, values):
+            dataframe[field] = value
+
+        print(dataframe)
+        pd.DataFrame(dataframe).to_csv(f"{self.path}/{table_name}.csv", index=False)
+
+    def start(self):
+        for table in self.tables:
+            fields, count = self.get_fields_and_count(table)
+            print("\n" + "=" * 30 + f" {table} " + "=" * 30)
+            values = [self.hk_ids[0:count]]
+            generator = Generator()
+            for field in self.data[table]["fields"]:
+                temp = []
+                if field == "AC" or field == "hk_id":
+                    continue
+                else:
+                    for i in range(count):
+                        temp.append(generator.generate(self.data[table][field]))
+                values.append(temp)
+            self.create_dataframe(table, fields, values)
+
+    def upload_to_oss(self):
+        print("\n" + self.get_time() + "-" * 30 + "  begin to upload files " + "-" * 30)
+        access_key_id = os.getenv('OSS_TEST_ACCESS_KEY_ID', 'LTAI5tAG24AcqCYzPvvw4ig8')
+        access_key_secret = os.getenv('OSS_TEST_ACCESS_KEY_SECRET', 'BWZCSGdF3XUeZh50knJap1t6BZ7GiQ')
+        bucket_name = os.getenv('OSS_TEST_BUCKET', 'apac-lab-ai-model')
+        endpoint = os.getenv('OSS_TEST_ENDPOINT', 'oss-cn-shenzhen.aliyuncs.com')
+
+        # 创建Bucket对象，所有Object相关的接口都可以通过Bucket对象来进行
+        bucket = oss2.Bucket(oss2.Auth(access_key_id, access_key_secret), endpoint, bucket_name)
+        tables = [csv for csv in os.listdir(self.path) if "csv" in csv]
+        for table in tables:
+            result = oss2.resumable_upload(bucket, f"dmp_mock_data/{table}", f"{self.path}/{table}")
+            print(f"{self.get_time()} {table} ---> upload status: {result.status}")
+
+
+def dmp_data_generation():
+    with open("dmp_tables_config.json", "r", encoding="utf-8") as f:
+        content = json.load(f)
+        mock = Mock(content["total_id"], content["data"], content["tables"])
+        mock.start()
+        mock.upload_to_oss()
+
+
+dmp_data_generation()
+
+
+def read_cdp_data(file):
+    path = "./data/dmp_sample_csv"
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    df = pd.read_csv(f"./dwh_raw_sample/{file}", header=None, error_bad_lines=False)
+    new_df = {}
+    for i, column in enumerate(df.loc[0, 0].split("|")):
+        temp = []
+        for data in df.loc[1:, 0]:
+            value = ""
+            if len(data.split("|")) > i:
+                value = data.split("|")[i]
+            temp.append(value)
+
+        print(file, temp)
+        new_df[column] = temp
+
+    pd.DataFrame(new_df).to_csv(f"./{path}/{file}", index=False)
+
+
+# for root, directories, files in os.walk("./dwh_raw_sample"):
+#     for file in files:
+#         print(file)
+#         read_cdp_data(file)
